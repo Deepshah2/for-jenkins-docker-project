beae792744d5abd6111fa6b60a99aa79f74686aa
Deepshah2 <deep.shah@dal.ca>
2018-05-19 13:10:58 -0700

    try

--- docker.sh
+++ docker.sh
@@ -6,17 +6,17 @@ echo "deep"
 
 #Run the container
 
-docker run -d -p 4000:80 friendlyhello
+#docker run -d -p 4000:80 friendlyhello
 
-echo "==== docker imageslists========="
-docker image ls | awk -F" " '{print $1}'
-echo "$Image"
-
-
-
-echo "===== the name of container========="
-docker container ls | awk -F" " '{print $1}'
-
-echo "$container"
+#echo "==== docker imageslists========="
+#docker image ls | awk -F" " '{print $1}'
+#echo "$Image"
 
 
+
+#echo "===== the name of container========="
+#docker container ls | awk -F" " '{print $1}'
+
+#echo "$container"
+
+#
