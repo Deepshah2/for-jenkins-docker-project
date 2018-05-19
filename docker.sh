5b599ed140a2a176bddbadd736d57ff42473517f
Deepshah2 <deep.shah@dal.ca>
2018-05-19 15:18:16 -0700

    testing

--- docker.sh
+++ docker.sh
@@ -3,19 +3,18 @@ WORKSPACE=/var/lib/jenkins
 export PYTHONPATH=$WORKSPACE:$PYTHONPATH
 echo "deep"
 echo "Hey this is my docker container"
+echo "Docker is running now"
 
-#Run the container
+docker run -d -p 4000:80 friendlyhello
 
-#docker run -d -p 4000:80 friendlyhello
-
-#echo "==== docker imageslists========="
-#docker image ls | awk -F" " '{print $1}'
+echo "==== docker imageslists========="
+docker image ls | awk -F" " '{print $1}'
 #echo "$Image"
 
 
 
-#echo "===== the name of container========="
-#docker container ls | awk -F" " '{print $1}'
+echo "===== the name of container========="
+docker container ls | awk -F" " '{print $1}'
 
 #echo "$container"
 
