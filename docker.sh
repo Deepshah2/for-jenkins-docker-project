56e74d83b72b04deff38905ece64796a36884231
Deepshah2 <deep.shah@dal.ca>
2018-05-19 13:07:47 -0700

    docker project

---
+++ docker.sh
@@ -0,0 +1,22 @@
+NPATH=/usr/bin/python
+WORKSPACE=/var/lib/jenkins
+export PYTHONPATH=$WORKSPACE:$PYTHONPATH
+echo "deep"
+
+
+#Run the container
+
+docker run -d -p 4000:80 friendlyhello
+
+echo "==== docker imageslists========="
+docker image ls | awk -F" " '{print $1}'
+echo "$Image"
+
+
+
+echo "===== the name of container========="
+docker container ls | awk -F" " '{print $1}'
+
+echo "$container"
+
+
